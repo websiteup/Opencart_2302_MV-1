2a637a4313831396e7e1c2839201868e13489107
condor2 <condor20_05@yahoo.it>
2019-02-04 23:41:33 +0200

    url alias deleted

--- upload/admin/model/catalog/information.php
+++ upload/admin/model/catalog/information.php
@@ -76,10 +76,6 @@ class ModelCatalogInformation extends Model {
 			}
 		}
 
-		if ($data['keyword']) {
-			$this->db->query("INSERT INTO " . DB_PREFIX . "url_alias SET query = 'information_id=" . (int)$information_id . "', keyword = '" . $this->db->escape($data['keyword']) . "'");
-		}
-
 		$this->cache->delete('information');
 	}
 
